80dd472c17d532e0f44e3975a120019b0ce3d065
Dragmine149 <dan@thebanners.uk>
2023-01-23 09:58:17 +0000

    Updated mypy stubgen to work now
    Also removed old stuff as in wrong directory

--- typings/tests/tests/Board_test.pyi
+++
@@ -1,7 +0,0 @@
-from . import PythonFunctions as PythonFunctions
-from _typeshed import Incomplete
-
-Board: Incomplete
-
-def rp_test_Size(x: int, y: int): ...
-def test_size() -> None: ...
--- typings/tests/tests/__init__.pyi
+++
@@ -1,8 +0,0 @@
-from src import PythonFunctions as PythonFunctions
-from src.PythonFunctions import Board as Board, Check as Check, CleanFolderData as CleanFolderData, Colours as Colours, Run as Run, SaveModules as SaveModules
-from src.PythonFunctions.Encryption import Encryption as Encryption
-from src.PythonFunctions.IsDigit import IsDigit as IsDigit
-from src.PythonFunctions.Message import Message as Message
-from src.PythonFunctions.PrintTraceback import PrintTraceback as PrintTraceback
-from src.PythonFunctions.Save import save as save
-from src.PythonFunctions.TerminalDisplay import Display as Display
