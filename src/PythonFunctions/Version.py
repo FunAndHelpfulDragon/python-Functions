1fe1557c7fe1972f496f9b2468028ab47ef33469
Dragmine149 <dan@thebanners.uk>
2023-02-15 01:42:49 +0000

    Removed auto upload + Files for 1.4.3

--- src/PythonFunctions/Version.py
+++ src/PythonFunctions/Version.py
@@ -47,7 +47,7 @@ def ReadLocal():
     Returns:
         str: Module version
     """
-    return "1.4.2"
+    return "1.4.3"
 
 
 url = "https://raw.githubusercontent.com/FunAndHelpfulDragon/python-Functions/main/Version.txt"
