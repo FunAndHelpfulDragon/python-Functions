a5ab9e00092b75646a5b8130595da6a8433b6907
Dragmine <dan@thebanners.uk>
2023-04-14 04:04:07 +0100

    small location issue fixes

--- src/PythonFunctions/Checks/Location.py
+++ src/PythonFunctions/Checks/Location.py
@@ -5,7 +5,7 @@ from PythonFunctions.Message import Message
 
 def check(value, **info):
     position = Location(value)
-    if checkInstances(None, *position):
+    if checkInstances(None, position):
         return None
 
     x = info.get('x')
@@ -19,4 +19,4 @@ def check(value, **info):
         Message.clear('Y value is too big!', timeS=1)
         return None
 
-    return True
+    return position
